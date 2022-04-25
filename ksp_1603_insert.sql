d07c2607afcf132eeb3f1edd2095b9f0797e20f7
Kiranpawar5155 <kiranpawar5155@gmail.com>
2022-04-25 08:56:38 +0530

    added 5 insert smt

---
+++ ksp_1603_insert.sql
@@ -0,0 +1,7 @@
+git clone -b feature/ksp-1603 https://github.com/kiranpawar5155/demo-01.git
+
+insert into employe
+insert into employe
+insert into employe
+insert into employe
+insert into employe
